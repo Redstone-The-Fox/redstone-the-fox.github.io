b2ecdda3391583c4faff19e1b7d32f4818d5199b
redstonethefox <zachnicholls19@gmail.com>
2024-10-12 12:36:07 +0100

    1

---
+++ script.js
@@ -0,0 +1,27 @@
+///////////////////
+// Starting Stuff
+///////////////////
+
+let clicks = 0;
+let clkdisp = document.getElementById('clickdisp').innerHTML;
+let cpc = 1;
+let cpcdisp = document.getElementById('cpcdisp').innerHTML;
+
+console.log("display='" + clkdisp + "'");
+console.log("clicks='" + clicks + "'");
+
+function updDisplay() {
+  clkdisp = "Clicks: " + clicks;
+  document.getElementById('clickdisp').innerHTML = clkdisp;
+  cpcdisp = "CPC: " + cpc;
+  document.getElementById('cpcdisp').innerHTML = cpcdisp;
+}
+
+updDisplay();
+console.log("display='" + clkdisp + "'");
+console.log("Console will be cleared in 3 seconds.");
+setTimeout(console.clear, 3000);
+
+clicks = Number(localStorage.getItem("clicks"));
+cpc = Number(localStorage.getItem("cpc"));
+updDisplay();
